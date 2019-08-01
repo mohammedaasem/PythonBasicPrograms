d9ce2cf2981f1ff90d47ab21a2d3942a49a19a89
mohammedaasem <imohammedaasem@gmail.com>
2019-08-01 17:33:30 +0530

    Version Data is Added.

---
+++ 2_inbuilt_function.py
@@ -0,0 +1,16 @@
+# type()
+a = 10
+print(type(a))  # Will return data_type of a
+
+# max()
+b = max(10, 20, 30, 20)  # will return maximum number for all arguments
+print(b)
+
+# min()
+c = min(10, 20, 30, 20)  # will return minimum number for all arguments
+print(c)
+
+# id()
+print(id(a))  # will return memory address of object
+print(id(b))
+print(id(c))
